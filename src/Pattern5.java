149a851750933e37753fbe3498279d9ebe61ca29
Yash Sharma <yash32860@gmail.com>
2024-11-22 22:38:32 +0530

    pattern5

---
+++ src/Pattern5.java
@@ -0,0 +1,14 @@
+public class Pattern5 {
+    public static void main(String[] args) {
+        int num =5;
+        for(int i=1;i<=num;i++){
+            for(int j=i;j<=num-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
