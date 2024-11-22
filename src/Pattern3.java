0cb7670d2672cc3008bc63d0af90f428f318fd39
Yash Sharma <yash32860@gmail.com>
2024-11-22 22:31:47 +0530

    this repo is to solve java questions

---
+++ src/Pattern3.java
@@ -0,0 +1,11 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int num =5;
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
