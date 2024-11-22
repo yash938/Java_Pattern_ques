bb1e16c184e2bc947e4991fef9241e14cfc5291f
Yash Sharma <yash32860@gmail.com>
2024-11-23 00:04:43 +0530

    pattern 9

--- src/Pattern9.java
+++ src/Pattern9.java
@@ -2,7 +2,7 @@ public class Pattern9 {
     public static void main(String[] args) {
         int num = 5;
 
-                int n = 5; // Number of rows
+                int n = 5;
 
                 for (int i = 0; i < n; i++) {
                     // Print spaces
@@ -13,7 +13,7 @@ public class Pattern9 {
                     for (int j = 0; j < (2 * (n - i) - 1); j++) {
                         System.out.print("*");
                     }
-                    // Move to the next line
+
                     System.out.println();
                 }
             }
